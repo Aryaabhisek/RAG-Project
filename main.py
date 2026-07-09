2172c8469270e50dc736abfd021638e1c4c4633a
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2026-07-09 19:07:54 +0530

    ChromaDB created sucessfully

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_mistralai import ChatMistralAI
+from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -17,6 +17,7 @@ template = ChatPromptTemplate.from_messages(
 )
 
 model = ChatMistralAI(model = "mistral-small-2506")
+embedding_model = MistralAIEmbeddings(model="mistral-embed")
 
 prompt = template.format_messages(data=docs[0].page_content)
 
